8ec2e4ef6189470f15417122d62d3ca322c8e8d4
esepgit <150302936+esepgit@users.noreply.github.com>
2024-01-22 18:29:02 -0300

    preliminar func ready

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,66 @@ import { useState } from'react';
 import './App.css';
 
 function App() {
-  const [operations, setOperations] = useState('');
   const [count, setCount] = useState('0');
+  const [auxNumber, setAuxNumber] = useState('0');
 
   const handleEraser = () => {setCount(count.slice(0, count.length - 1))}
-  const handleClear = () => {setCount('0'); setOperations('');}
+  const handleClear = () => {setCount('0')}
 
   const handleClickDigit = (digit) => {
-    if (count.length >= 15) {
-      const aux = count;
-      setTimeout(() => {
-        setCount(aux)
-      }, 1000);
-      setCount('DIGIT LIMIT')
+    if (count === '0') {
+      setCount(digit)
+      setAuxNumber(digit)
     } else {
-        if (count === '0' || count === '-' || count === '+' || count === 'X' || count === '/') {
-          setOperations(operations + digit)
-          setCount(digit)
-        } else {
-          setOperations(operations + digit)
-          setCount(count + digit)
-        }
+      setCount(count.toString() + digit)
+      setAuxNumber(auxNumber.concat(digit))
     }
   }
 
   const handleClickOperation = (op) => {
-    setOperations(operations + op)
-    setCount(op)
+    if (!count.includes('X') && !count.includes('+') && !count.includes('-') && !count.includes('/'))
+    setCount(count.toString() + op)
+    setAuxNumber('')
+  }
 
+  const handleClickTotal = () => {
+    if (count.includes('+') || count.includes('-') || count.includes('/') || count.includes('X')) {
+      if (/.+[+\-X/].+/g.test(count)) {
+        const first = parseFloat(
+          count.slice(0, count.indexOf(count.match(/[+\-X/]/)))
+        ).toFixed(4);
+        const second = parseFloat(
+          count.slice(count.indexOf(count.match(/[+\-X/]/)) + 1)
+        ).toFixed(4);
+        const operation = count.match(/[+\-X/]/);
+
+        if (operation == "+") {
+          setCount(String(Number(first) + Number(second)));
+        } else if (operation == "-") {
+          setCount(String(Number(first) - Number(second)));
+        } else if (operation == "X") {
+          setCount(String(Number(first) * Number(second)));
+        } else if (operation == '/') {
+          setCount(String(Number(first) / Number(second)));
+        } else {
+          console.log(operation)
+        }
+      }
+    }
+    
+    setAuxNumber('0')
+  }
+
+  const handleClickDecimal = () => {
+    if (!auxNumber.includes('.')) {
+      setCount(count.concat('.'))
+      setAuxNumber(auxNumber.concat('.'))
+    }
   }
 
   return (
     <div id='calculator'>
       <div id='display-container'>
-        <div id="operations">{...operations}</div>
         <div id="display">{count}</div>
       </div>
       <div id="keys">
@@ -55,8 +81,8 @@ function App() {
         <button id='three' className='digit' onClick={() => handleClickDigit('3')}>3</button>
         <button id='add' className='operation' onClick={() => handleClickOperation('+')}>+</button>
         <button id='zero' className='zero' onClick={() => handleClickDigit('0')}>0</button>
-        <button id='decimal' className='digit'>.</button>
-        <button id='equals' className='equals'>=</button>
+        <button id='decimal' className='digit' onClick={handleClickDecimal}>.</button>
+        <button id='equals' className='equals' onClick={handleClickTotal}>=</button>
       </div>
     </div>
   )
